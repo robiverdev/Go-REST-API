3f9bb8c7075f26d0e2d439ccf5fa6b897e340c61
Robi Verdev <robi.verdev1@gmail.com>
2025-04-14 19:09:51 +0200

    Added GET and function for single event

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"example.com/rest-api/db"
 	"example.com/rest-api/models"
@@ -14,6 +15,7 @@ func main() {
 	server := gin.Default() // set up Engine (HTTP server) -> pointer *gin.Engine
 
 	server.GET("/events", getEvents) // GET request
+	server.GET("/events/:id",getEvent) // GET a single event
 	server.POST("/events", createEvent) // POST request
 
 	server.Run(":8080") // listening for incoming requests on localhost:8080
@@ -26,6 +28,21 @@ func getEvents(context *gin.Context) {
 		return
 	}
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event ID"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event"})
+		return
+	}
+	context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context) {
